d31caa66cdf399af477107aaa93558372d855105
Sam Hermes <sam.hermes@wustl.edu>
2018-11-11 12:09:09 -0600

    Upgrade to Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 'use strict';
- 
+
 var gulp = require('gulp'),
   sass = require('gulp-sass'),
   sourcemaps = require('gulp-sourcemaps'),
   autoprefixer = require('gulp-autoprefixer'),
   livereload = require('gulp-livereload');
- 
+
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['>0.25%'],
       cascade: false
@@ -18,11 +18,11 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./'))
     .pipe(livereload());
 });
- 
+
 gulp.task('watch', function () {
-	livereload.listen();
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  livereload.listen();
+  gulp.watch('./sass/**/*.scss', gulp.series('sass'));
   gulp.watch('**/*.php', livereload.reload);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', gulp.parallel('sass', 'watch'));
